4f703260db1a17eb982e8732de754c7cffe0dd91
Chukwubuikem-Onwuchuruba <ikemonwuchuruba2004@gmail.com>
2023-10-23 21:47:06 -0500

    latest changes

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,76 +1,67 @@
-import connectTODB from "@/database";
+import connectToDB from "@/database";
 import User from "@/models/user";
 import { hash } from "bcryptjs";
 import Joi from "joi";
 import { NextResponse } from "next/server";
 
-
-
-
 const schema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
-    role: Joi.string().required()
-})
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  role: Joi.string().required(),
+});
 
-export const dynamic = 'force-dynamic';
-
-
-
+export const dynamic = "force-dynamic";
 
 export async function POST(req) {
+  await connectToDB();
 
-    await connectTODB();
+  const { name, email, password, role } = await req.json();
+  //validate the schema
 
-    const { name, email, password, role } = await req.json();
-    //validate the schema
+  const { error } = schema.validate({ name, email, password, role });
 
-    const { error } = schema.validate(name, email, password, role)
+  if (error) {
+    console.log(error);
+    return NextResponse.json({
+      success: false,
+      message: error.details[0].message,
+    });
+  }
 
-    if (error) {
-        console.log(error)
+  try {
+    //check if the user is exists or not
+
+    const isUserAlreadyExists = await User.findOne({ email });
+
+    if (isUserAlreadyExists) {
+      return NextResponse.json({
+        success: false,
+        message: "User is already exists. Please try with different email.",
+      });
+    } else {
+      const hashPassword = await hash(password, 12);
+
+      const newlyCreatedUser = await User.create({
+        name,
+        email,
+        password: hashPassword,
+        role,
+      });
+
+      if (newlyCreatedUser) {
         return NextResponse.json({
-            success: false,
-            message: error.details[0].message
-        })
+          success: true,
+          message: "Account created successfully.",
+        });
+      }
     }
+  } catch (error) {
+    console.log("Error while new user registration. Please try again");
 
-    try {
-
-        //check if the user exists or not
-
-        const doesUserAlreadyExist = await User.findOne({ email })
-
-        if (doesUserAlreadyExist) {
-
-            return NextResponse.json({
-                success: false,
-                message: 'User already exists. Please try with a different email.'
-            })
-        } else {
-            const hashPassword = await hash(password, 12);
-
-            const newlyCreatedUser = await User.create({
-                name, email, password: hashPassword, role
-            })
-
-            if (newlyCreatedUser) {
-                return NextResponse.json({
-                    success: true,
-                    message: 'Account created successfully.'
-                })
-            }
-        }
-    }
-    catch (error) {
-        console.log('Error while registering new user. Please try again.')
-
-        return NextResponse.json({
-            success: false,
-            message: 'Something went wrong! Please try again later.'
-        })
-    }
-
-
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong! Please try again later",
+    });
+  }
 }
