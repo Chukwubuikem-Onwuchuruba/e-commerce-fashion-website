6a44b4b4d3210a10e99eb17ea1eab1686e8d5e84
Chukwubuikem-Onwuchuruba <ikemonwuchuruba2004@gmail.com>
2023-11-04 00:30:37 -0500

    latest changes

--- src/database/index.js
+++ src/database/index.js
@@ -1,16 +1,20 @@
 import mongoose from "mongoose";
 
-
-
 const configOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
 const connectToDB = async () => {
-    const connectionUrl = 'mongodb+srv://kolomental:Mff11c@cluster0.yhdxygk.mongodb.net/'
+  const connectionUrl =
+  'mongodb+srv://kolomental:Mff11c@cluster0.yhdxygk.mongodb.net/';
 
-    mongoose.connect(connectionUrl, configOptions).then(() => console.log('E-commerce Fashion Website Database connected successfully!')).catch((err) => console.log(`Getting Error from DB connection ${err.message}`))
-}
+  mongoose
+    .connect(connectionUrl, configOptions)
+    .then(() => console.log("Ecommerce database connected successfully!"))
+    .catch((err) =>
+      console.log(`Getting Error from DB connection ${err.message}`)
+    );
+};
 
-export default connectTODB
+export default connectToDB;
